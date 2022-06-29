671bd1d1e3577c1528cef7cc16742264591d5c04
Courtnee McCollum <courtneemeaghan@gmail.com>
2022-06-29 10:39:33 -0500

    Retain testimonial display for larger screens when resizing

--- script.js
+++ script.js
@@ -14,12 +14,50 @@
   const testimonials = [...testimonialsContainer.querySelectorAll('.testimonial')];
   const [prevControl, nextControl] = [...document.querySelector('#testimonial-controls').children];
   const progressCircles = [...document.querySelector('.testimonial-progress').children];
+
+  // Resets testimonials for larger screen sizes
+  window.addEventListener('resize', () => {
+    if ([...testimonialsContainer.querySelectorAll('.testimonial')].length === 3) return;
+
+    if (window.matchMedia('(min-width:860px)').matches) {
+      testimonials.forEach((testimonial) => {
+        if ([...testimonialsContainer.children].includes(testimonial)) {
+          testimonialsContainer.removeChild(testimonial);
+        }
+      });
+      
+      testimonials.forEach((testimonial) => {
+        testimonialsContainer.insertBefore(testimonial, prevControl.parentElement);
+      });
+    }
+  });
   
-  if (window.matchMedia('(min-width:640px)')) {
+  if (window.matchMedia('(min-width:640px)').matches) {
     let indices = [0, 1];
     let activeTestimonials = [testimonials[indices[0]], testimonials[indices[1]]];
 
-    nextControl.addEventListener('click', (e) => {
+    prevControl.addEventListener('click', () => {
+      progressCircles[indices[0]].removeAttribute('id');
+
+      indices[0] = testimonials[indices[0]-1] === undefined ? testimonials.length - 1 : indices[0] - 1;
+      indices[1] = testimonials[indices[1]-1] === undefined ? testimonials.length - 1 : indices[1] - 1;
+
+      activeTestimonials = [testimonials[indices[0]], testimonials[indices[1]]];
+
+      testimonials.forEach((testimonial) => {
+        if ([...testimonialsContainer.children].includes(testimonial)) {
+          testimonialsContainer.removeChild(testimonial);
+        }
+      });
+
+
+      activeTestimonials.forEach((testimonial) => {
+        testimonialsContainer.insertBefore(testimonial, prevControl.parentElement);
+      });
+      progressCircles[indices[0]].id = 'active-testimonial';
+    });
+
+    nextControl.addEventListener('click', () => {
       progressCircles[indices[0]].removeAttribute('id');
 
       indices[0] = testimonials[indices[0]+1] === undefined ? 0 : indices[0] + 1;
@@ -39,7 +77,7 @@
       });
       progressCircles[indices[0]].id = 'active-testimonial';
     });
-  } else if (window.matchMedia('(min-width:0px)')) {
+  } else if (window.matchMedia('(min-width:0px)').matches) {
     let index = 0;
 
     prevControl.addEventListener('click', () => {
